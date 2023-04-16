571c07603c5bc7e641b179f0b7a960e1c8232dda
Mauritz Marais <mauritz303@gmail.com>
2023-04-16 18:55:01 +0200

    separated front end code, added bcrypt and passport local

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const passport = require('passport');
 
-// routes
-const routes = require('./routes/routes');
+// authentication srategy* jwt
 
 require('dotenv').config()
 // Connect DB
@@ -22,9 +23,14 @@ db.on("error", console.error.bind(console, "mongo connection error"));
 const app = express();
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 
 // routes
+const routes = require('./routes/routes');
 app.use('/', routes);
+
+// Plug in the JWT strategy as a middleware so only verified users can access this route.*
 
 
 app.listen(3000, () => {
