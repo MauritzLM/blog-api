8f96966dd5c09a20d3dba7422ec97a771e6cca1f
Mauritz Marais <mauritz303@gmail.com>
2023-04-17 17:29:32 +0200

    removed password method

--- models/admin.js
+++ models/admin.js
@@ -3,14 +3,14 @@ const bcrypt = require('bcryptjs');
 
 const Schema = mongoose.Schema;
 
-const adminSchema = new Schema({
+const AdminSchema = new Schema({
     username: { type: String, required: true },
     password: { type: String, required: true },
     email: { type: String, required: true }
 });
 
 // hash password (from digital ocean tutorial)
-adminSchema.pre('save', async function (next) {
+AdminSchema.pre('save', async function (next) {
     const admin = this;
     const hash = await bcrypt.hash(this.password, 10);
 
@@ -18,12 +18,4 @@ adminSchema.pre('save', async function (next) {
     next();
 });
 
-// method to compare passwords
-UserSchema.methods.isValidPassword = async function (password) {
-    const user = this;
-    const compare = await bcrypt.compare(password, user.password);
-
-    return compare;
-}
-
-module.exports = mongoose.model('Admin', adminSchema);
+module.exports = mongoose.model('Admin', AdminSchema);
