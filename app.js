0f1fbae10e893138a8e1e2e52ea9fa73439aef3f
Mauritz Marais <mauritz303@gmail.com>
2023-04-19 16:17:12 +0200

    installed cookie parser

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser')
 
 require('dotenv').config()
 // Connect DB
@@ -20,6 +21,7 @@ db.on("error", console.error.bind(console, "mongo connection error"));
 const app = express();
 
 app.use(cors());
+app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 
