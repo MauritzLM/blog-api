864d639c4e177d274eff27b91707bb1869bbe782
Mauritz Marais <mauritz303@gmail.com>
2023-06-07 16:33:24 +0200

    added options for cors

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 // const bodyParser = require('body-parser');
-// const formidableMiddleware = require('express-formidable');
 // const multer = require('multer');
 
+// configure cors
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    credentials: true
+};
 
 require('dotenv').config()
 // Connect DB
@@ -23,11 +27,10 @@ db.on("error", console.error.bind(console, "mongo connection error"));
 
 const app = express();
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-// app.use(formidableMiddleware());
 
 
 // routes
